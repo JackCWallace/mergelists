df551a4f3e23a62a55f9875bed828d376d1db8cf
Jack Wallace <jackcwallace51@gmail.com>
2020-12-18 23:51:31 -0500

    add post

--- backend/src/models/model.js
+++ backend/src/models/model.js
@@ -4,7 +4,10 @@ class Model {
   constructor(table) {
     this.pool = pool;
     this.table = table;
-    this.pool.on('error', (err, client) => `Error, ${err}, on idle client${client}`);
+    this.pool.on(
+      'error',
+      (err, client) => `Error, ${err}, on idle client${client}`
+    );
   }
 
   async select(columns, clause) {
@@ -12,6 +15,14 @@ class Model {
     if (clause) query += clause;
     return this.pool.query(query);
   }
-}
 
+  async insertWithReturn(columns, values) {
+    const query = `
+          INSERT INTO ${this.table} (${columns})
+          VALUES (${values})
+          RETURNING id, ${columns}
+      `;
+    return this.pool.query(query);
+  }
+}
 export default Model;
--- backend/src/utils/queries.js
+++ backend/src/utils/queries.js
@@ -1,11 +1,11 @@
-export const createMessageTable = `
-DROP TABLE IF EXISTS messages;
-CREATE TABLE IF NOT EXISTS messages (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR DEFAULT '',
-  message VARCHAR NOT NULL
-  )
-  `;
+export const createMessageTable = "\
+DROP TABLE IF EXISTS messages;\
+CREATE TABLE IF NOT EXISTS messages (\
+  id SERIAL PRIMARY KEY,\
+  name VARCHAR DEFAULT '',\
+  message VARCHAR NOT NULL\
+  )\
+  ";
 
 export const insertMessages = `
 INSERT INTO messages(name, message)
@@ -13,4 +13,4 @@ VALUES ('chidimo', 'first message'),
       ('orji', 'second message')
 `;
 
-export const dropMessagesTable = 'DROP TABLE messages';
+export const dropMessagesTable = `DROP TABLE messages`;
