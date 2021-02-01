48b70c7d31620e1ebf14fbdde9c4b1c151067af9
Jack Wallace <jackcwallace51@gmail.com>
2021-01-31 22:25:49 -0500

    after testing with front end down

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { indexPage, messagesPage, addMessage } from '../controllers';
 import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
-indexRouter.get('/', indexPage);
+indexRouter.get('/', messagePage);
 indexRouter.get('/messages', messagesPage);
 indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
