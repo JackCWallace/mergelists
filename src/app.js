497db2c75d50743985dc794c5b5acb2718844325
Jack Wallace <jackcwallace51@gmail.com>
2021-01-31 22:01:12 -0500

    fixed order of app use

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(logger('dev'));
 var static_pages = path.join(__dirname, '..', 'client', 'build');
 console.log(static_pages);
-app.use(express.static(static_pages), indexRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/', indexRouter);
+app.use(express.static(static_pages), indexRouter);
 
 app.use((err, req, res, next) => {
     res.status(400).json({error: err.stack});
